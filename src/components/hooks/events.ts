9ae1e8ec8e900c1bdc90ac41bcbca38d73719603
lvliqi <lvliqi_d@163.com>
2025-06-29 16:44:04 +0800

    feat: up

--- src/components/hooks/events.ts
+++ src/components/hooks/events.ts
@@ -10,15 +10,6 @@ import { BarId } from '@/types/gantt-bar';
 export const useEvents = (ganttEntity: Gantt, store:{
   bus: ReturnType<typeof useBus>
 }) => {
-  ganttEntity.bus.on(GanttBusEvents.BAR_POS_CHANGE, (ids) => {
-    store.bus.emit(Events.BAR_CHANGE, ids);
-    store.bus.emit(Events.BAR_POS_CHANGE, ids);
-  });
-
-  ganttEntity.bus.on(GanttBusEvents.ATTACHED_BAR_POS_CHANGE, (ids) => {
-    store.bus.emit(Events.ATTACHED_BAR_CHANGE, ids);
-    store.bus.emit(Events.ATTACHED_BAR_POS_CHANGE, ids);
-  });
 
   ganttEntity.config.on(GanttConfig.EVENTS.DRAGGABLE_CHANGE, (val) => {
     store.bus.emit(Events.DRAGGABLE_CHANGE, val);
@@ -32,11 +23,6 @@ export const useEvents = (ganttEntity: Gantt, store:{
     store.bus.emit(Events.CHECKABLE_CHANGE, val);
   });
 
-  store.bus.on(Events.BAR_CHANGE, asyncFragmentation<BarId[]>(async (options:BarId[][]) => {
-    const ids = uniq(flatten(options));
-    store.bus.emit(Events.BAR_CHANGE_FRAGMENTATION, ids);
-    return [];
-  }));
 
   ganttEntity.bus.on(GanttBusEvents.GROUP_HEIGHT_CHANGE, (data) => {
     store.bus.emit(Events.GROUP_CHANGE, [data.groupId]);
@@ -53,6 +39,22 @@ export const useEvents = (ganttEntity: Gantt, store:{
     store.bus.emit(Events.DATE_GRID_CHANGE);
     store.bus.emit(Events.WORK_TIME_GRID_CHANGE);
   });
+
+  ganttEntity.bus.on(GanttBusEvents.BAR_POS_CHANGE, (ids) => {
+    store.bus.emit(Events.BAR_CHANGE, ids);
+    store.bus.emit(Events.BAR_POS_CHANGE, ids);
+  });
+
+  ganttEntity.bus.on(GanttBusEvents.ATTACHED_BAR_POS_CHANGE, (ids) => {
+    store.bus.emit(Events.ATTACHED_BAR_CHANGE, ids);
+    store.bus.emit(Events.ATTACHED_BAR_POS_CHANGE, ids);
+  });
+
+  store.bus.on(Events.BAR_CHANGE, asyncFragmentation<BarId[]>(async (options:BarId[][]) => {
+    const ids = uniq(flatten(options));
+    store.bus.emit(Events.BAR_CHANGE_FRAGMENTATION, ids);
+    return [];
+  }));
   
   store.bus.on(Events.GROUP_EXPAND_CHANGE, () => {
     store.bus.emit(Events.SCROLL_CHANGE);
